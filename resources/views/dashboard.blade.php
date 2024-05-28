9a97d7b9490657391f48a94509202ae9e825ac09
17Jared <jaredmutua17@gmail.com>
2024-05-28 14:57:11 +0300

    DashboardUpdate

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,27 +1,29 @@
 @extends('Shared.layout')
 @include('newNav')
 @section('content')
-    <div class="row container-fluid justify-content-center ">
-        @for ($i = 0; $i < 10; $i++)
-            <div class="col-10 col-md-6 col-lg-4 p-0  card text-center m-1">
-                <img class="img-fluid card-img-top w-100"
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuA7safnMxSkszvObGxvPrtB-6nxXbcgOkVg&usqp=CAU"
-                    alt="image">
-                <div class="card-body">
-                    <h4 class="card-title h4">
-                        Uzuri Grounds
-                    </h4>
-                    <p class="card-text">Location <br>
-                        Capaity <br>
-                        Charges <br>
-                        Contact info <br>
-                        Policy <br>
-                    </p>
-                    <div class="card-footer"><a class="btn btn-dark mt-1 rounded">Make booking</a>
-                    </div>
+    @auth
+        <div class="row container-fluid justify-content-center ">
+            @for ($i = 0; $i < 10; $i++)
+                <div class="p-0 m-1 text-center col-10 col-md-6 col-lg-4 card">
+                    <img class="img-fluid card-img-top w-100"
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuA7safnMxSkszvObGxvPrtB-6nxXbcgOkVg&usqp=CAU"
+                        alt="image">
+                    <div class="card-body">
+                        <h4 class="card-title h4">
+                            Uzuri Grounds
+                        </h4>
+                        <p class="card-text">Location <br>
+                            Capaity <br>
+                            Charges <br>
+                            Contact info <br>
+                            Policy <br>
+                        </p>
+                        <div class="card-footer"><a class="mt-1 rounded btn btn-dark">Make booking</a>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
-        @endfor
-    </div>
+            @endfor
+        </div>
+    @endauth
 @endsection
